52a5f5cf18e919f9ce5a8a7742f1ed93617ec26e
Srikanthkotla1969 <88161670+Srikanthkotla1969@users.noreply.github.com>
2024-06-09 13:15:42 +0530

    Update elasticip.tf

--- elasticip.tf
+++ elasticip.tf
@@ -1,8 +1,8 @@
-provider "aws"{
+/*provider "aws"{
     region="us-east-1"
 }
 
 resource "aws_eip" "first_eip" {
     domain="vpc" #domain = “vpc” specifies the domain of the Elastic IP. 
     #“vpc” indicates that it’s a VPC (Virtual Private Cloud) EIP. This is the default value, and it’s appropriate for most VPC scenarios.
-}
+}*/
