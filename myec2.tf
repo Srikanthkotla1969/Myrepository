c007d7c2c0ddde48e3bc45c37af9166345004cbe
Srikanthkotla1969 <88161670+Srikanthkotla1969@users.noreply.github.com>
2024-06-09 13:25:27 +0530

    Update myec2.tf

--- myec2.tf
+++ myec2.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "myec2_tfcloud" {
     ami="ami-0440d3b780d96b29d"
     instance_type = "t2.micro"
+
+    tags = {
+        name = "sentinel-example"
+    }
 }
